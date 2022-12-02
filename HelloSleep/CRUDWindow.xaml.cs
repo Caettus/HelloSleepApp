99a3902290d3154de4d1dd8c120fac25926db6b7
Caettus <vincentscheffer2@gmail.com>
2022-12-02 12:13:16 +0100

    Crud window toegevoegd, load baby data en load user data buttons doen nog niks

---
+++ HelloSleep/CRUDWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloSleep
+{
+    /// <summary>
+    /// Interaction logic for CRUDWindow.xaml
+    /// </summary>
+    public partial class CRUDWindow : Window
+    {
+        public CRUDWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UserDataBn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BabyDataBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
